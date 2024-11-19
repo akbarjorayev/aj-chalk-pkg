c63746871ca30d03af18becaa71857bc6078be6b
Akbar <akbarjonjorayev001@gmail.com>
2024-11-19 23:13:43 +0500

    pkg import fixed

--- aj_chalk/utils/hex_to_rgb.py
+++
@@ -1,7 +0,0 @@
-def hex_to_rgb(hex_color="#fff"):
-    hex_color = hex_color.lstrip('#')
-    
-    if len(hex_color) == 3:
-        hex_color = ''.join([c*2 for c in hex_color])
-    
-    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
